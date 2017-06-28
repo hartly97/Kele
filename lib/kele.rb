6df007657b849d4af1fbf89a7d1df0884af06b1c
hartly97 <mhartwikitree@gmail.com>
2017-06-28 16:34:50 -0500

    roadmaps:chkpts and rdmap

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap.rb'
 
 class Kele
   include HTTParty
+  include Roadmap
   attr_reader :token, :response
 
   def initialize(email, password)
@@ -18,16 +20,17 @@ class Kele
       raise @response.message or 'Connection failed' unless @response.code == 200
 
       @token = @response.parsed_response["auth_token"]
-    end
+  end
 
   def get_me
       response = self.class.get("/users/me", headers: {"authorization" => token})
       @current_user = JSON.parse(response.body)
-    end
+  end
 
     #mentor_id 523127
   def get_mentor_availability(mentor_id)
-   response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => token})
-    @current_user = JSON.parse(response.body)
-    end
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => token})
+    #@current_user = JSON.parse(response.body)
+    @get_mentor_availability = JSON.parse(response.body)
   end
+end
